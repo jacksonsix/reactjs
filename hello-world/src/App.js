debfb5131877439b3ca9b24a12ca8e70245b2d39
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-26 21:11:29 -0400

    Update App.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -2,6 +2,12 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+class Ts extends Component{
+  render(){
+    return(<h3> Ts value is {this.props.value} </h3>);
+  }
+}
+
 class App extends Component {
   render() {
     return (
@@ -10,6 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React 888</h2>
           <h3>hehe</h3>
+          <Ts value = {90} />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
