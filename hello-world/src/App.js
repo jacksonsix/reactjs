9467bc3373d461ffbf3d68df6de8af5dfd646423
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-26 21:12:37 -0400

    Update App.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -16,7 +16,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React 888</h2>
           <h3>hehe</h3>
-          <Ts value = {90} />
+          <Ts value={90} />
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
