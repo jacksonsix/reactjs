bee42dc787640e29523b5a529878ac62f21f8061
jacksonsix <jacksonsixmailbox@gmail.com>
2017-03-26 21:16:04 -0400

    Update App.js

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -17,6 +17,7 @@ class App extends Component {
           <h2>Welcome to React 888</h2>
           <h3>hehe</h3>
           <Ts value={90} />
+          <p> page after </p>
         </div>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
